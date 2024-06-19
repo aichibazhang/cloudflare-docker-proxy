40ce0713cd1cf9cf8a6449876771d50685c93ee4
aichibazhang <597410004@qq.com>
2024-06-19 18:05:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+ 
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -42,8 +44,20 @@ async function handleRequest(request) {
       }
     );
   }
+  
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
+  }
+  
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
+  
   if (url.pathname == "/v2/") {
     const newUrl = new URL(upstream + "/v2/");
     const headers = new Headers();
@@ -151,16 +165,4 @@ async function fetchToken(wwwAuthenticate, scope, authorization) {
     headers.set("Authorization", authorization);
   }
   return await fetch(url, { method: "GET", headers: headers });
-}
-
-import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
 }
